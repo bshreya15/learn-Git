50d26c5854ba81984d7dab984081338f11fbf9e1
Shreya Bakshi <i.bakshi.shreya@gmail.com>
2021-10-10 21:20:24 +0530

    changes in diff.java file

--- diff.java
+++ diff.java
@@ -1,3 +1,11 @@
+import jdk.javadoc.internal.tool.Main;
+
 public class diff {
-    
+    public static void main(String[] args){
+        int a = 5;
+        int b = 3;
+
+        System.out.println("the sum is : "+(a-b));
+
+    }
 }
